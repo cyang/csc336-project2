f5cb3aa6a76dff38e5767d05b2520fcf9ddc272e
cyang <cyang001@citymail.cuny.edu>
2016-12-20 19:22:11 -0500

    Add index investment

--- investmentStrategies.php
+++ investmentStrategies.php
@@ -6,6 +6,8 @@ if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
 
+define("CONSTANT_BUDGET", 10000.0);
+
 // Strategy 1: Buy and hold selectively 
 echo "1. Buy and hold selectively<br><br>";
 $q_1 = "SELECT startStock.INSTRUMENT_ID, startStock.TRADE_DATE as startDate,
@@ -32,7 +34,7 @@ if ($r_1->num_rows > 0) {
 }
 
 
-$budget = 10000.0;
+$budget = CONSTANT_BUDGET;
 echo "Starting budget: ". $budget . "<br><br>";
 $profit = 0.0;
 
@@ -49,5 +51,29 @@ while ($budget > 0) {
 }
 
 echo "<br>Budget left over: " . $budget . "<br>"; 
-echo "Profit: " . $profit;
-?>
+echo "Profit: " . $profit . "<br><br>";
+
+// 3. Index investment
+echo "3. Index investment:<br>";
+
+// For the first index:
+$index1TotalStart = 0.0;
+$index1TotalEnd = 0.0;
+for ($i = 0; $i < 500; $i++) {
+	$index1TotalStart += $stockArray[$i][0];
+	$index1TotalEnd += $stockArray[$i][1];	
+}
+$index1PercentChange = ($index1TotalStart-$index1TotalEnd)/$index1TotalStart;
+echo "Index 1 percent change: " . $index1PercentChange . "<br>";
+echo "Budget change: " . $index1PercentChange*CONSTANT_BUDGET . "<br><br>"; 
+
+// For the second index:
+$index2TotalStart = 0.0;
+$index2TotalEnd = 0.0;
+for ($i = 500; $i < 1000; $i++) {
+	$index2TotalStart += $stockArray[$i][0];
+	$index2TotalEnd += $stockArray[$i][1];	
+}
+$index2PercentChange = ($index2TotalStart-$index2TotalEnd)/$index2TotalStart;
+echo "Index 2 percent change: " . $index2PercentChange . "<br>";
+echo "Budget change: " . $index2PercentChange*CONSTANT_BUDGET . "<br>";?>
