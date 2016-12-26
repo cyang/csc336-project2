2a298d4f744a1640083deb045ebc5b518842ceb1
Jesse Feinman <jesse.feinman@gmail.com>
2016-12-26 04:43:01 -0500

    WIP
    Cleaned up code
    Added print statements with timestamps for runtime speed analysis
    reduced run time
    Average run time is 700 seconds
    does not produce a profit, currently it looses most of the investment.

--- movingaverage.php
+++ movingaverage.php
@@ -1,8 +1,10 @@
 <?php
 require "config.php";
 
-ini_set('memory_limit','2000M');
-ini_set('max_execution_time', 3600);
+ini_set('memory_limit','4000M');
+ini_set('max_execution_time', 6000);
+ob_implicit_flush(true);
+$starttime = microtime(true);
 
 // Connection
 $db = new mysqli($host, $username, $password, "stockmarket");
@@ -11,12 +13,17 @@ if ($db->connect_error) {
 }
 
 // Strategy 1: Buy and hold selectively
-echo "1. Calculate Averages<br><br>";
+echo "1. Calculate Averages, time: ".(microtime(true) - $starttime)." <br><br>";
+echo "2. Running db query, time: ".(microtime(true) - $starttime)." <br><br>";
+flush();
+ob_flush();
 $q_1 = "SELECT INSTRUMENT_ID, TRADE_DATE, CLOSE_PRICE FROM STOCK_HISTORY order by TRADE_DATE";
 $r_1 = $db->query($q_1);
 
 $stockArray = array();
-
+echo "3. Processing query results into array, time: ".(microtime(true) - $starttime)." <br><br>";
+flush();
+ob_flush();
 if ($r_1->num_rows > 0) {
         while($row = $r_1->fetch_assoc()) {
                 if(array_key_exists($row["INSTRUMENT_ID"], $stockArray)){
@@ -29,8 +36,11 @@ if ($r_1->num_rows > 0) {
 
 $cashStart = 10000.0;
 $cash = $cashStart;
-echo "Starting budget: ". $cash . "<br><br>";
 
+echo "4. Starting budget: ".$cash." time: ".(microtime(true) - $starttime)." <br><br>";
+echo "5. Calculating 50 and 200 day averages, time: ".(microtime(true) - $starttime)." <br><br>";
+flush();
+ob_flush();
 $avgArray = array();
 for($i = 0; $i < count($stockArray); $i++){
   $priceArray = $stockArray[$i];
@@ -38,64 +48,55 @@ for($i = 0; $i < count($stockArray); $i++){
   for($day = 201; $day < count($priceArray); $day++){
     $day50 = array_sum(array_slice ( $priceArray, $day-51, 50 ))/50;
     $day200 = array_sum(array_slice ( $priceArray, $day-201, 200 ))/200;
-//    echo("inst: $i day: $day 50: $day50 200: $day200<br>");
-  if($day50 > $day200){
-      $avgInstArray[$day] = array(1, $priceArray[$day]);
-    }else {
-      $avgInstArray[$day] = array(0, $priceArray[$day]);
-    }}
+    $avgInstArray[$day] = array(($day50 > $day200 ? 1:0), $priceArray[$day]);
+    }
   $avgArray[$i] = $avgInstArray;
 }
 
-$minDay = array();
-for($day = 0; $day < count($avgArray[0]); $day++){
-  $dayPrice = array();
-  for($i = 0; $i < count($avgArray); $i++){
-    if($avgArray[$i][$day][0] == 1){
-       array_push($dayPrice,$avgArray[$i][$day][1]);
-  }}
-  array_push($minDay, min($dayPrice));
-}
-
+echo "6. Adding stocks to buy/sell list for each day, time: ".(microtime(true) - $starttime)." <br><br>";
+flush();
+ob_flush();
 $buy = array();
 $sell = array();
-for($day = 0; $day < count($avgArray[0]); $day++){
+for($day = 201; $day < count($avgArray[0]); $day++){
   $buyDay = array();
   $sellDay = array();
-  for($i = 0; $i < count($avgArray); $i++){
+  foreach($avgArray as $i => $arr){
     if($avgArray[$i][$day][0] == 1){
-      array_push($buyDay,array($i, $avgArray[$i][$day][1]));
+      $buyDay[$i]=$avgArray[$i][$day][1];
   }else{
-      array_push($sellDay,array($i, $avgArray[$i][$day][1]));
+      $sellDay[$i]=$avgArray[$i][$day][1];
   }}
-  array_push($buy, $buyDay);
-  array_push($sell, $sellDay);
+  $buy[$day] =  $buyDay;
+  $sell[$day] =  $sellDay;
 }
 
 $holdings = array();
+echo "7. Running algorith,<br> for each day sell all stocks that match criteria and then buy randomly from the pool of stocks that meet buy criteria, time: ".(microtime(true) - $starttime)." <br><br>";
+flush();
+ob_flush();
+for($day = 201; $day < count($avgArray[0]); $day++){
+  foreach($sell[$day] as $i => $price){
+    if(array_key_exists($i, $holdings)){
+      $cash += $price * $holdings[$i];
+      unset($holdings[$i]);
+  }}
 
-for($day = 0; $day < count($avgArray[0]); $day++){
-  for($stock = 0; $stock < count($sell[$day]); $stock++){
-    if(in_array($sell[$day][$stock][0], array_column($holdings,0))){
-      $qtIndex = array_search($sell[$day][$stock][0], array_column($holdings,0));
-      $cash += $sell[$day][$stock][1] * $holdings[$qtIndex][1];
-    }
-  }
-while($cash > $min){
-  $stock = array_rand($buy[$day]);
-  $cash -= $buy[$day][$stock[1]];
-  if(array_key_exists($stock[0], $holdings)){
-    $holdings[$stock[0]][1] += 1;
-  }else {
-    $holdings[$stock[0]][1] = 1;
-  }
+  while($cash > 250 and count($buy[$day]) > 0 ){
+    $i = array_rand($buy[$day]);
+    $cash -= $buy[$day][$i];
+    if(array_key_exists($i, $holdings)){
+      $holdings[$i] += 1;
+    }else{
+      $holdings[$i] = 1;
+}}}
+
+echo "8. Calculating profits, time: ".(microtime(true) - $starttime)." <br><br>";
+flush();
+ob_flush();
+foreach($holdings as $i => $qty){
+  $cash += end($stockArray[$i]) * $qty;
 }
-}
-$lastDay =  count($avgArray[0]) - 1;
-  for($stock = 0; $stock < count($sell[$lastDay]); $stock++){
-      $qtIndex = array_search($holdings[$stock][0], array_column($stockArray,0));
-      $cash += $holdings[$stock][1] * $stockArray[$qtIndex][$lastDay];
-  }
 $profit = $cash - $cashStart;
-echo "Profit: " . $profit;
+echo "9. Profit: " . $profit . " time: ".(microtime(true) - $starttime)." <br><br>";
 ?>
