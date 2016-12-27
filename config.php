dfe97d992c408d15d6bf20cd361cd168fafd6b3b
Connie Liu <heyconnie123@gmail.com>
2016-12-26 20:27:58 -0500

    momentum-unfinished

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host = "134.74.126.107";
+$host = "134.74.126.104";
 $username = "USERNAME";
 $password = "PASSWORD";
 $team_database = "F16336team5";
@@ -7,5 +7,5 @@ $team_database = "F16336team5";
 $db = new mysqli($host, $username, $password, $team_database);
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
-} 
+}
 ?>
